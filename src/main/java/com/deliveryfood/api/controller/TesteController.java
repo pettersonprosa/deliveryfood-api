d7d516538ad59de02ce3194994d19faa85db37ab
petterson <petterson@email.com>
2025-03-06 18:25:26 -0300

    criando consultas com query methods

---
+++ src/main/java/com/deliveryfood/api/controller/TesteController.java
@@ -0,0 +1,31 @@
+package com.deliveryfood.api.controller;
+
+import com.deliveryfood.domain.model.Cozinha;
+import com.deliveryfood.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/testes")
+public class TesteController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping("/cozinhas/por-nome")
+    public List<Cozinha> cozinhasPorNome(@RequestParam("nome") String nome) {
+        return cozinhaRepository.nome(nome);
+    }
+
+    @GetMapping("/cozinhas/unica-por-nome")
+    public Optional<Cozinha> cozinhaPorNome(String nome) {
+        return cozinhaRepository.findByNome(nome);
+    }
+
+}
--- src/main/java/com/deliveryfood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/deliveryfood/domain/repository/CozinhaRepository.java
@@ -5,10 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
-//    List<Cozinha> consultaPorNome(String nome);
-
+    List<Cozinha> nome(String nome);
+    Optional<Cozinha> findByNome(String nome);
 }
