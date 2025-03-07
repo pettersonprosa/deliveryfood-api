6a9ed228baaaf642d4d06b28bb43b4ef0eb72cab
petterson <petterson@email.com>
2025-03-07 10:59:53 -0300

    externalizando consultas JPQL para um arquivo XML

--- src/main/java/com/deliveryfood/api/controller/TesteController.java
+++ src/main/java/com/deliveryfood/api/controller/TesteController.java
@@ -41,7 +41,7 @@ public class TesteController {
 
     @GetMapping("/restaurantes/por-nome")
     public List<Restaurante> restaurantesPorTaxaFrete(String nome, Long cozinhaId) {
-        return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
+        return restauranteRepository.consultarPorNome(nome, cozinhaId);
     }
 
 }
--- src/main/java/com/deliveryfood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/deliveryfood/domain/repository/RestauranteRepository.java
@@ -2,6 +2,8 @@ package com.deliveryfood.domain.repository;
 
 import com.deliveryfood.domain.model.Restaurante;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
@@ -13,4 +15,7 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
     List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 
     List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha);
+
+    List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinha);
+
 }
