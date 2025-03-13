c3ee9f5467ba1e163118ecb6f236ea7b80220758
petterson <petterson@email.com>
2025-03-13 14:54:37 -0300

    Estendendo ResponseStatusException

--- src/main/java/com/deliveryfood/domain/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/deliveryfood/domain/exception/EntidadeNaoEncontradaException.java
@@ -1,11 +1,17 @@
 package com.deliveryfood.domain.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.server.ResponseStatusException;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Entidade não encontrada")
-public class EntidadeNaoEncontradaException extends RuntimeException {
+//@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Entidade não encontrada")
+public class EntidadeNaoEncontradaException extends ResponseStatusException {
+
+    public EntidadeNaoEncontradaException(HttpStatusCode status, String mensagem) {
+        super(status, mensagem);
+    }
+
     public EntidadeNaoEncontradaException(String message) {
-        super(message);
+        this(HttpStatus.NOT_FOUND, message);
     }
 }
