20c46c9a93a965dad7eda6c6424fbfdd99c65f1d
petterson <petterson@email.com>
2025-03-20 18:47:26 -0300

    Validando o corpo da resposta HTTP

--- src/test/java/com/deliveryfood/CadastroCozinhaIT.java
+++ src/test/java/com/deliveryfood/CadastroCozinhaIT.java
@@ -1,5 +1,8 @@
 package com.deliveryfood;
 
+import static io.restassured.RestAssured.given;
+
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -18,7 +21,7 @@ class CadastroCozinhaIT {
     public void deveRetornarStatus200_QuandoConsultarCozinhas() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 
-        RestAssured.given()
+        given()
             .basePath("/cozinhas")
             .port(port)
             .accept(ContentType.JSON)
@@ -27,5 +30,21 @@ class CadastroCozinhaIT {
         .then()
             .statusCode(HttpStatus.OK.value());
     }
+
+    @Test
+    public void deveConter5Cozinhas_QuandoConsultarCozinhas() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+        given()
+            .basePath("/cozinhas")
+            .port(port)
+            .accept(ContentType.JSON)
+        .when()
+            .get()
+        .then()
+            .body("", Matchers.hasSize(5))
+            .body("nome", Matchers.hasItems("Indiana", "Tailandesa"));
+    }
+
 
 }
