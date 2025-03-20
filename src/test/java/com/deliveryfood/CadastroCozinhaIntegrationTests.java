6a97bd34ca188f86227f78c688f3ca9ac8b392fb
petterson <petterson@email.com>
2025-03-20 10:55:32 -0300

    Criando e rodando um teste de integração com Spring Boot, JUnit e AssertJ

---
+++ src/test/java/com/deliveryfood/CadastroCozinhaIntegrationTests.java
@@ -0,0 +1,46 @@
+package com.deliveryfood;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.deliveryfood.domain.model.Cozinha;
+import com.deliveryfood.domain.service.CadastroCozinhaService;
+
+import jakarta.validation.ConstraintViolationException;
+
+@SpringBootTest
+class CadastroCozinhaIntegrationTests {
+
+    @Autowired
+    private CadastroCozinhaService cadastroCozinha;
+
+    @Test
+    public void testarCadastroCozinhaComSucesso() {
+        // cenário
+        Cozinha novaCozinha = new Cozinha();
+        novaCozinha.setNome("Chinesa");
+
+        // ação
+        novaCozinha = cadastroCozinha.salvar(novaCozinha);
+
+        // validação
+        assertThat(novaCozinha).isNotNull();
+        assertThat(novaCozinha.getId()).isNotNull();
+    }
+
+    @Test
+    public void testarCadastroCozinhaSemNome() {
+        Cozinha novaCozinha = new Cozinha();
+        novaCozinha.setNome(null);
+
+       ConstraintViolationException erroEsperado =
+               Assertions.assertThrows(ConstraintViolationException.class, () -> {
+                   cadastroCozinha.salvar(novaCozinha);
+               });
+
+    }
+
+}
