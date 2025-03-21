922a17d5380e08e3a851b03e68b92e000657e73b
petterson <petterson@email.com>
2025-03-21 09:40:48 -0300

    Configurando um banco de testes e usando `@TestPropertySource`

--- src/test/java/com/deliveryfood/CadastroCozinhaIT.java
+++ src/test/java/com/deliveryfood/CadastroCozinhaIT.java
@@ -10,10 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
+@TestPropertySource("/application-test.properties")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CadastroCozinhaIT {
 
