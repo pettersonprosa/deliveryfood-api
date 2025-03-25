c8f6ba9f475049e2b44d68fbcefa86c444d565eb
petterson <petterson@email.com>
2025-03-25 16:42:23 -0300

    Implementando os endpoints de ativação e inativação de restaurantes

--- src/main/java/com/deliveryfood/api/controller/RestauranteController.java
+++ src/main/java/com/deliveryfood/api/controller/RestauranteController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -78,5 +79,18 @@ public class RestauranteController {
             throw new NegocioException(e.getMessage());
         }
     }
+
+    @PutMapping("/{restauranteId}/ativo")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void ativar (@PathVariable Long restauranteId) {
+        cadastroRestaurante.ativar(restauranteId);
+    }
+
+    @DeleteMapping("/{restauranteId}/ativo")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void inativar (@PathVariable Long restauranteId) {
+        cadastroRestaurante.inativar(restauranteId);
+    }
+
 
 }
--- src/main/java/com/deliveryfood/domain/model/Restaurante.java
+++ src/main/java/com/deliveryfood/domain/model/Restaurante.java
@@ -8,7 +8,6 @@ import java.util.List;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import com.deliveryfood.core.validation.Groups;
 import com.deliveryfood.core.validation.ValorZeroIncluiDescricao;
 
 import jakarta.persistence.Column;
@@ -22,12 +21,6 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
-import jakarta.validation.groups.ConvertGroup;
-import jakarta.validation.groups.Default;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -42,24 +35,20 @@ public class Restaurante {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // @NotBlank
     @Column(nullable = false)
     private String nome;
 
-    // @NotNull
-    // @PositiveOrZero
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
-    // @Valid
-    // @ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)
-    // @NotNull
     @ManyToOne //(fetch = FetchType.LAZY)
     @JoinColumn(name="cozinha_id", nullable = false) // o "name" por padrão é <nome_variavel>_id, ou seja, ficaria cozinha_id. Nesse caso, então poderia deixar só @JoinColumn
     private Cozinha cozinha;
 
     @Embedded
     private Endereco endereco;
+
+    private Boolean ativo = Boolean.TRUE;
 
     @CreationTimestamp
     @Column(nullable = false, columnDefinition = "datetime")
@@ -77,4 +66,14 @@ public class Restaurante {
 
     @OneToMany(mappedBy = "restaurante")
     private List<Produto> produtos = new ArrayList<>();
+
+    public void ativar() {
+        setAtivo(true);
+    }
+
+    public void inativar() {
+        setAtivo(false);
+    }
+
+
 }
