31ff683b7db2d13668149c7d25051cfafdee972e
petterson <petterson@email.com>
2025-03-26 09:00:41 -0300

    Adicionando endereço no modelo da representação do recurso de restaurante

---
+++ src/main/java/com/deliveryfood/api/model/CidadeResumoModel.java
@@ -0,0 +1,14 @@
+package com.deliveryfood.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CidadeResumoModel {
+
+    private Long id;
+    private String nome;
+    private String estado;
+    
+}
--- src/main/java/com/deliveryfood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/deliveryfood/core/modelmapper/ModelMapperConfig.java
@@ -4,11 +4,24 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.deliveryfood.api.model.EnderecoModel;
+import com.deliveryfood.domain.model.Endereco;
+
 @Configuration
 public class ModelMapperConfig {
 
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
+        var modelMapper = new ModelMapper();
+        
+        var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(
+            Endereco.class, EnderecoModel.class);
+        
+        enderecoToEnderecoModelTypeMap.<String>addMapping(
+            enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(), 
+            (enderecoModelDest, value) -> enderecoModelDest.getCidade().setEstado(value)); 
+            //enderecoModelDest -> Objeto do tipo EnderecoModel && value -> retorno da primeira expressão lambda (enderecoSrc.getCidade().getEstado().getNome())
+
+        return modelMapper;
     }
 }
