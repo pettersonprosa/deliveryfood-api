d10821421ffb1705e96829550dd6d72ad62f397f
petterson <petterson@email.com>
2025-03-27 11:48:04 -0300

    Desafio: implementando os endpoints de produtos

---
+++ src/main/java/com/deliveryfood/domain/service/CadastroProdutoService.java
@@ -0,0 +1,26 @@
+package com.deliveryfood.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.deliveryfood.domain.exception.ProdutoNaoEncontradoException;
+import com.deliveryfood.domain.model.Produto;
+import com.deliveryfood.domain.repository.ProdutoRepository;
+
+@Service
+public class CadastroProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @Transactional
+    public Produto salvar(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public Produto buscarOuFalhar(Long restauranteId, Long produtoId) {
+        return produtoRepository.findById(restauranteId, produtoId)
+            .orElseThrow(() -> new ProdutoNaoEncontradoException(restauranteId, produtoId));
+    }
+}
