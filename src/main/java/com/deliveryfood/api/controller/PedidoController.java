1b0b9983cde827f08c79b92e0d450771dca38eee
petterson <petterson@email.com>
2025-04-01 16:12:52 -0300

    Desafio: implementando paginação e ordenação de pedidos

--- src/main/java/com/deliveryfood/api/controller/PedidoController.java
+++ src/main/java/com/deliveryfood/api/controller/PedidoController.java
@@ -1,18 +1,18 @@
 package com.deliveryfood.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,8 +30,6 @@ import com.deliveryfood.domain.repository.PedidoRepository;
 import com.deliveryfood.domain.repository.filter.PedidoFilter;
 import com.deliveryfood.domain.service.EmissaoPedidoService;
 import com.deliveryfood.infrastructure.repository.spec.PedidoSpecs;
-import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
-import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 import jakarta.validation.Valid;
 
@@ -55,10 +53,16 @@ public class PedidoController {
     private PedidoInputDisassembler pedidoInputDisassembler;
 
     @GetMapping
-    public List<PedidoResumoModel> pesquisar(PedidoFilter filtro) {
-        List<Pedido> todosPedidos = pedidoRepository.findAll(PedidoSpecs.usandoFiltro(filtro));
+    public Page<PedidoResumoModel> pesquisar(@PageableDefault(size=10) Pageable pageable, PedidoFilter filtro) {
+        Page<Pedido> pedidosPage = pedidoRepository.findAll(PedidoSpecs.usandoFiltro(filtro), pageable);
+
+        List<PedidoResumoModel> pedidosResumoModel = pedidoResumoModelAssembler.toCollectionModel(
+                pedidosPage.getContent());
+
+        Page<PedidoResumoModel> pedidosResumoModelPages = new PageImpl<>(
+                pedidosResumoModel, pageable, pedidosPage.getTotalElements());
         
-        return pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
+        return pedidosResumoModelPages;
     }
 
     @GetMapping("/{codigoPedido}")
