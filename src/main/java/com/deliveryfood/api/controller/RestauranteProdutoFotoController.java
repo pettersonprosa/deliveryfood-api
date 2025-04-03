bb96c4e856241bfeb5644c732b1bb2cb01e2de28
petterson <petterson@email.com>
2025-04-03 10:29:06 -0300

    Validando o tamanho máximo do arquivo

--- src/main/java/com/deliveryfood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/deliveryfood/api/controller/RestauranteProdutoFotoController.java
@@ -10,13 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.deliveryfood.api.model.input.FotoProdutoInput;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/restaurantes/{restauranteId}/produtos/{produtoId}/foto")
 public class RestauranteProdutoFotoController {
 
     @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, 
-            FotoProdutoInput fotoProdutoInput) {
+            @Valid FotoProdutoInput fotoProdutoInput) {
 
         String nomeArquivo = UUID.randomUUID().toString() + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
 
--- src/main/java/com/deliveryfood/api/model/input/FotoProdutoInput.java
+++ src/main/java/com/deliveryfood/api/model/input/FotoProdutoInput.java
@@ -2,6 +2,10 @@ package com.deliveryfood.api.model.input;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import com.deliveryfood.core.validation.FileSize;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,6 +13,10 @@ import lombok.Setter;
 @Setter
 public class FotoProdutoInput {
 
+    @NotNull
+    @FileSize(max = "650KB")
     private MultipartFile arquivo;
+    
+    @NotBlank
     private String descricao;
 }
