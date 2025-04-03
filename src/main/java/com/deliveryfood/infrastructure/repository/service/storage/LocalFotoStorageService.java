236b4f43eb9895cd35b920b85ede2f561b459ebb
petterson <petterson@email.com>
2025-04-03 16:05:02 -0300

    Implementando o serviço de armazenagem de fotos no disco local

---
+++ src/main/java/com/deliveryfood/infrastructure/repository/service/storage/LocalFotoStorageService.java
@@ -0,0 +1,33 @@
+package com.deliveryfood.infrastructure.repository.service.storage;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
+
+import com.deliveryfood.domain.service.FotoStorageService;
+
+@Service
+public class LocalFotoStorageService implements FotoStorageService {
+
+    @Value("${deliveryfood.storage.local.diretorio-fotos}")
+    private Path diretorioFotos;
+
+    @Override
+    public void armazenar(NovaFoto novaFoto) {
+        try {
+            Path arquivoPath = getArquivoPath(novaFoto.getNomeArquivo());
+
+            FileCopyUtils.copy(novaFoto.getInputStream(), Files.newOutputStream(arquivoPath));
+        } catch (Exception e) {
+            throw new StorageException("Não foi possível armazenar arquivo.", e);
+        }
+    }
+
+    private Path getArquivoPath(String nomeArquivo) {
+        return diretorioFotos.resolve(Path.of(nomeArquivo));
+    }
+
+}
---
+++ src/main/java/com/deliveryfood/infrastructure/repository/service/storage/StorageException.java
@@ -0,0 +1,13 @@
+package com.deliveryfood.infrastructure.repository.service.storage;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public StorageException(String message) {
+        super(message);
+    }
+    
+}
