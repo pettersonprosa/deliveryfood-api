23e122a6fcea33c7b9c504731bed2375f4bbe2da
petterson <petterson@email.com>
2025-04-04 09:25:55 -0300

    Servindo arquivos de fotos pela API

--- src/main/java/com/deliveryfood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/deliveryfood/api/controller/RestauranteProdutoFotoController.java
@@ -1,9 +1,12 @@
 package com.deliveryfood.api.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,10 +17,12 @@ import org.springframework.web.multipart.MultipartFile;
 import com.deliveryfood.api.assembler.FotoProdutoModelAssembler;
 import com.deliveryfood.api.model.FotoProdutoModel;
 import com.deliveryfood.api.model.input.FotoProdutoInput;
+import com.deliveryfood.domain.exception.EntidadeNaoEncontradaException;
 import com.deliveryfood.domain.model.FotoProduto;
 import com.deliveryfood.domain.model.Produto;
 import com.deliveryfood.domain.service.CadastroProdutoService;
 import com.deliveryfood.domain.service.CatalogoFotoProdutoService;
+import com.deliveryfood.domain.service.FotoStorageService;
 
 import jakarta.validation.Valid;
 
@@ -34,12 +39,15 @@ public class RestauranteProdutoFotoController {
     @Autowired
     private FotoProdutoModelAssembler fotoProdutoModelAssembler;
 
+    @Autowired
+    private FotoStorageService fotoStorage;
+
     @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public FotoProdutoModel atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, 
+    public FotoProdutoModel atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId,
             @Valid FotoProdutoInput fotoProdutoInput) throws IOException {
 
         Produto produto = cadastroProduto.buscarOuFalhar(restauranteId, produtoId);
-        
+
         MultipartFile arquivo = fotoProdutoInput.getArquivo();
 
         FotoProduto foto = new FotoProduto();
@@ -54,12 +62,28 @@ public class RestauranteProdutoFotoController {
         return fotoProdutoModelAssembler.toModel(fotoSalva);
     }
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public FotoProdutoModel buscar(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
         FotoProduto fotoProduto = catalogoFotoProduto.buscarOuFalhar(restauranteId, produtoId);
 
         return fotoProdutoModelAssembler.toModel(fotoProduto);
     }
 
+    @GetMapping(produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<InputStreamResource> servirFoto(@PathVariable Long restauranteId,
+            @PathVariable Long produtoId) {
+        try {
+            FotoProduto fotoProduto = catalogoFotoProduto.buscarOuFalhar(restauranteId, produtoId);
+
+            InputStream inputStream = fotoStorage.recuperar(fotoProduto.getNomeArquivo());
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.IMAGE_JPEG)
+                    .body(new InputStreamResource(inputStream));
+
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
