f6d2b4120f58958c05652384e544e2bc139805c8
petterson <petterson@email.com>
2025-04-10 08:39:54 -0300

    Observando e reagindo a Domain Events

---
+++ src/main/java/com/deliveryfood/domain/listener/NotificacaoClientePedidoConfirmadoListener.java
@@ -0,0 +1,31 @@
+package com.deliveryfood.domain.listener;
+
+import com.deliveryfood.domain.service.EnvioEmailService;
+import com.deliveryfood.domain.service.EnvioEmailService.Mensagem;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import com.deliveryfood.domain.event.PedidoConfirmadoEvent;
+import com.deliveryfood.domain.model.Pedido;
+
+@Component
+public class NotificacaoClientePedidoConfirmadoListener {
+
+    @Autowired
+    private EnvioEmailService envioEmail;
+
+    @EventListener
+    public void aoConfirmarPedido(PedidoConfirmadoEvent event) {
+        Pedido pedido = event.getPedido();
+        var mensagem = Mensagem.builder()
+                .assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
+                .corpo("pedido-confirmado.html")
+                .variavel("pedido", pedido)
+                .destinatario(pedido.getCliente().getEmail())
+                .build();
+
+        envioEmail.enviar(mensagem);
+    }
+}
