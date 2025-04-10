a81d1c04585733625a4b9ff4e30c2d0159d3ebf7
petterson <petterson@email.com>
2025-04-10 15:58:26 -0300

    Habilitando CORS globalmente no projeto da API

--- src/main/java/com/deliveryfood/api/controller/RestauranteController.java
+++ src/main/java/com/deliveryfood/api/controller/RestauranteController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,7 +30,6 @@ import com.fasterxml.jackson.annotation.JsonView;
 
 import jakarta.validation.Valid;
 
-@CrossOrigin(origins = "*") // origins = "*" é o default -> ele libera o acesso para todos os origins
 @RestController
 @RequestMapping("/restaurantes")
 public class RestauranteController {
---
+++ src/main/java/com/deliveryfood/core/web/WebConfig.java
@@ -0,0 +1,17 @@
+package com.deliveryfood.core.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*"); // o default são requisições simples -> GET, HEAD, POST
+                // .allowedOrigins("*") //"*" já e default
+                // .maxAge(30); //medido em segundos
+    }
+}
