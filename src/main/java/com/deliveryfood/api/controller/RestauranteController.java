c4ac3dd5b3d6023bdae1532d0423a5716e290343
petterson <petterson@email.com>
2025-04-10 14:30:44 -0300

    Habilitando CORS em controladores e métodos com @CrossOrigin

--- src/main/java/com/deliveryfood/api/controller/RestauranteController.java
+++ src/main/java/com/deliveryfood/api/controller/RestauranteController.java
@@ -3,9 +3,8 @@ package com.deliveryfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,6 +31,7 @@ import com.fasterxml.jackson.annotation.JsonView;
 
 import jakarta.validation.Valid;
 
+@CrossOrigin(origins = "*") // origins = "*" é o default -> ele libera o acesso para todos os origins
 @RestController
 @RequestMapping("/restaurantes")
 public class RestauranteController {
@@ -51,56 +51,15 @@ public class RestauranteController {
 
     @JsonView(RestauranteView.Resumo.class)
     @GetMapping
-    public ResponseEntity<List<RestauranteModel>> listar() {
-        List<RestauranteModel> restauranteModel = restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
-
-        return ResponseEntity.ok()
-        .header(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, " http://127.0.0.1:5500")
-        .body(restauranteModel);
+    public List<RestauranteModel> listar() {
+        return restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
     }
 
-    // @JsonView(RestauranteView.ApenasNome.class)
-    // @GetMapping(params = "projecao=apenas-nome")
-    // public List<RestauranteModel> listarApenasNomes() {
-    //     return listar();
-    // }
-
-    // @GetMapping
-    // public MappingJacksonValue listar(@RequestParam(required = false) String projecao) {
-    //     List<Restaurante> restaurantes = restauranteRepository.findAll();
-    //     List<RestauranteModel> restauranteModels = restauranteModelAssembler.toCollectionModel(restaurantes);
-
-    //     MappingJacksonValue restaurantesWrapper = new MappingJacksonValue(restauranteModels);
-
-    //     restaurantesWrapper.setSerializationView(RestauranteView.Resumo.class);
-
-    //     if ("apenas-nome".equals(projecao)) {
-    //         restaurantesWrapper.setSerializationView(RestauranteView.ApenasNome.class);
-    //     } else if ("completo".equals(projecao)) {
-    //         restaurantesWrapper.setSerializationView(null);
-    //     }
-
-    //     return restaurantesWrapper;
-    // }
-
-    // @GetMapping
-    // public List<RestauranteModel> listar() {
-    //     return restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
-    // }
-
-    // @JsonView(RestauranteView.Resumo.class)
-    // @GetMapping(params = "projecao=resumo")
-    // public List<RestauranteModel> listarResumido() {
-    //     return restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
-    // }
-
-    // @JsonView(RestauranteView.ApenasNome.class)
-    // @GetMapping(params = "projecao=apenas-nome")
-    // public List<RestauranteModel> listarApenasNomes() {
-    //     return restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
-    // }
-
-    
+    @JsonView(RestauranteView.ApenasNome.class)
+    @GetMapping(params = "projecao=apenas-nome")
+    public List<RestauranteModel> listarApenasNomes() {
+        return listar();
+    }
 
     @GetMapping({ "/{restauranteId}" })
     public RestauranteModel buscar(@PathVariable Long restauranteId) {
