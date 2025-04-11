3504c44b4ae5da98b991d7348f06c4b718c2cdba
petterson <petterson@email.com>
2025-04-11 10:02:37 -0300

    Desafio: implementando uma requisição DELETE com JavaScript

--- src/main/resources/static/client.js
+++ src/main/resources/static/client.js
@@ -3,8 +3,8 @@ function consultar() {
     url: "http://127.0.0.1:8080/formas-pagamento",
     type: "GET",
 
-    success: function(response) {
-      preencherTabela(response)
+    success: function (response) {
+      preencherTabela(response);
     }
   });
 }
@@ -21,13 +21,13 @@ function cadastrar() {
     data: formaPagamentoJson,
     contentType: "application/json",
 
-    success: function(response) {
+    success: function (response) {
       alert("Forma de pagamento cadastrada com sucesso!");
       consultar();
     },
 
-    error: function(error) {
-      if (error.status == 400) {
+    error: function (error) {
+      if (error.status >= 400 && error.status <= 499) {
         var problem = JSON.parse(error.responseText);
         alert(problem.userMessage);
       } else {
@@ -37,21 +37,52 @@ function cadastrar() {
   });
 }
 
+function excluir(formaPagamento) {
+  // alert("Implemente aqui para excluir forma de pagamento de ID " + formaPagamento.id);
+  var url = "http://127.0.0.1:8080/formas-pagamento/" + formaPagamento.id;
+
+  $.ajax({
+    url: url,
+    type: "DELETE",
+
+    success: function (response) {
+      consultar();
+      alert("Forma de pagamento removida com sucesso!");
+    },
+
+    error: function (error) {
+      if (error.status >= 400 && erros.status <= 499) {
+        var problem = JSON.parse(error.responseText);
+        alert(problem.userMessage);
+      } else {
+        alert("Erro ao remover forma de pagamento!");
+      }
+    }
+  });
+}
+
 function preencherTabela(formasPagamento) {
   $("#tabela tbody tr").remove();
 
-  $.each(formasPagamento, function(i, formaPagamento) {
+  $.each(formasPagamento, function (i, formaPagamento) {
     var linha = $("<tr>");
+
+    var linkAcao = $("<a href='#'>")
+      .text("Excluir")
+      .click(function (event) {
+        event.preventDefault();
+        excluir(formaPagamento);
+      });
 
     linha.append(
       $("<td>").text(formaPagamento.id),
-      $("<td>").text(formaPagamento.descricao)
+      $("<td>").text(formaPagamento.descricao),
+      $("<td>").append(linkAcao)
     );
 
     linha.appendTo("#tabela");
   });
 }
 
-
 $("#btn-consultar").click(consultar);
-$('#btn-cadastrar').click(cadastrar);
+$("#btn-cadastrar").click(cadastrar);
