eab9434211ffeee5beeee445ed25727903f9c760
petterson <petterson@email.com>
2025-04-14 16:25:03 -0300

    Implementando requisições condicionais com Shallow ETags

--- src/main/java/com/deliveryfood/core/web/WebConfig.java
+++ src/main/java/com/deliveryfood/core/web/WebConfig.java
@@ -1,6 +1,8 @@
 package com.deliveryfood.core.web;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,5 +15,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("*"); // o default são requisições simples -> GET, HEAD, POST
                 // .allowedOrigins("*") //"*" já e default
                 // .maxAge(30); //medido em segundos
+    }
+
+    @Bean
+    public ShallowEtagHeaderFilter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
     }
 }
