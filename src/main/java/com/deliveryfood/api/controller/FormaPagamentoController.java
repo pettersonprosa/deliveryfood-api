20f33860cd05aa2209cbcabf4b14d423202a8ed4
petterson <petterson@email.com>
2025-04-14 17:04:07 -0300

    Adicionando outras diretivas de Cache-Control na resposta HTTP

--- src/main/java/com/deliveryfood/api/controller/FormaPagamentoController.java
+++ src/main/java/com/deliveryfood/api/controller/FormaPagamentoController.java
@@ -51,6 +51,11 @@ public class FormaPagamentoController {
 
         return ResponseEntity.ok()
                 .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+                // .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+                // .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePrivate())
+                // .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePublic())
+                // .cacheControl(CacheControl.noCache())
+                // .cacheControl(CacheControl.noStore())
                 .body(formasPagamentosModel);
     }
 
