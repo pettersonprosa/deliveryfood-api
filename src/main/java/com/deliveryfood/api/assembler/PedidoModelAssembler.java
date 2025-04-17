875799b18f29f7667dc42c93fbea55e5bc632b17
petterson <petterson@email.com>
2025-04-17 14:54:25 -0300

    Desafio: adicionando template variables do filtro de pedidos

--- src/main/java/com/deliveryfood/api/assembler/PedidoModelAssembler.java
+++ src/main/java/com/deliveryfood/api/assembler/PedidoModelAssembler.java
@@ -25,55 +25,61 @@ import com.deliveryfood.domain.model.Pedido;
 @Component
 public class PedidoModelAssembler extends RepresentationModelAssemblerSupport<Pedido, PedidoModel> {
 
-    @Autowired
-    private ModelMapper modelMapper;
+	@Autowired
+	private ModelMapper modelMapper;
 
-    public PedidoModelAssembler() {
-    		super(PedidoController.class, PedidoModel.class);
-    }
+	public PedidoModelAssembler() {
+		super(PedidoController.class, PedidoModel.class);
+	}
 
-    @Override
-    public PedidoModel toModel(Pedido pedido) {
-        PedidoModel pedidoModel = createModelWithId(pedido.getCodigo(), pedido);
+	@Override
+	public PedidoModel toModel(Pedido pedido) {
+		PedidoModel pedidoModel = createModelWithId(pedido.getCodigo(), pedido);
 
-        modelMapper.map(pedido, pedidoModel);
+		modelMapper.map(pedido, pedidoModel);
 
-        TemplateVariables pageVariables = new TemplateVariables(
-                new TemplateVariable("page", VariableType.REQUEST_PARAM),
-                new TemplateVariable("size", VariableType.REQUEST_PARAM),
-                new TemplateVariable("sort", VariableType.REQUEST_PARAM));
+		TemplateVariables pageVariables = new TemplateVariables(
+				new TemplateVariable("page", VariableType.REQUEST_PARAM),
+				new TemplateVariable("size", VariableType.REQUEST_PARAM),
+				new TemplateVariable("sort", VariableType.REQUEST_PARAM));
+
+		TemplateVariables filtroVariables = new TemplateVariables(
+				new TemplateVariable("clienteId", VariableType.REQUEST_PARAM),
+				new TemplateVariable("restauranteId", VariableType.REQUEST_PARAM),
+				new TemplateVariable("dataCriacaoInicio", VariableType.REQUEST_PARAM),
+				new TemplateVariable("dataCriacaoFim", VariableType.REQUEST_PARAM));
 
 		String pedidoUrl = linkTo(PedidoController.class).toUri().toString();
 
-        pedidoModel.add(Link.of(UriTemplate.of(pedidoUrl, pageVariables), "pedidos"));
+		pedidoModel.add(Link.of(UriTemplate.of(pedidoUrl, pageVariables.concat(filtroVariables)), "pedidos"));
 
-        // pedidoModel.add(linkTo(PedidoController.class)
-        // .withRel("pedidos"));
+		// pedidoModel.add(linkTo(PedidoController.class)
+		// .withRel("pedidos"));
 
-        pedidoModel.getRestaurante().add(linkTo(methodOn(RestauranteController.class)
-                .buscar(pedido.getRestaurante().getId()))
-                .withSelfRel());
+		pedidoModel.getRestaurante().add(linkTo(methodOn(RestauranteController.class)
+				.buscar(pedido.getRestaurante().getId()))
+				.withSelfRel());
 
-        pedidoModel.getCliente().add(linkTo(methodOn(UsuarioController.class)
-                .buscar(pedido.getCliente().getId()))
-                .withSelfRel());
+		pedidoModel.getCliente().add(linkTo(methodOn(UsuarioController.class)
+				.buscar(pedido.getCliente().getId()))
+				.withSelfRel());
 
-        // Foi passado null no segundo argumento do método buscar, porque é
-        // indiferente para construção da URL do recurso de forma de pagamento
-        pedidoModel.getFormaPagamento().add(linkTo(methodOn(FormaPagamentoController.class)
-                .buscar(pedido.getFormaPagamento().getId(), null))
-                .withSelfRel());
+		// Foi passado null no segundo argumento do método buscar, porque é
+		// indiferente para construção da URL do recurso de forma de pagamento
+		pedidoModel.getFormaPagamento().add(linkTo(methodOn(FormaPagamentoController.class)
+				.buscar(pedido.getFormaPagamento().getId(), null))
+				.withSelfRel());
 
-        pedidoModel.getEnderecoEntrega().getCidade().add(linkTo(methodOn(CidadeController.class)
-                .buscar(pedido.getEnderecoEntrega().getCidade().getId()))
-                .withSelfRel());
+		pedidoModel.getEnderecoEntrega().getCidade().add(linkTo(methodOn(CidadeController.class)
+				.buscar(pedido.getEnderecoEntrega().getCidade().getId()))
+				.withSelfRel());
 
-        pedidoModel.getItens().forEach(item -> {
-            item.add(linkTo(methodOn(RestauranteProdutoController.class)
-                    .buscar(pedidoModel.getRestaurante().getId(), item.getProdutoId()))
-                    .withSelfRel());
-        });
+		pedidoModel.getItens().forEach(item -> {
+			item.add(linkTo(methodOn(RestauranteProdutoController.class)
+					.buscar(pedidoModel.getRestaurante().getId(), item.getProdutoId()))
+					.withSelfRel());
+		});
 
-        return pedidoModel;
-    }
+		return pedidoModel;
+	}
 }
