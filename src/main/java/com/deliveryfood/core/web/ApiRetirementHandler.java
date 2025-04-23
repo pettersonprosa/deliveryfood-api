4473cadcee6965dc7c1b7decf746c1a86c35b2f9
petterson <petterson@email.com>
2025-04-23 15:11:49 -0300

    Desligando uma versão da API

--- src/main/java/com/deliveryfood/core/web/ApiRetirementHandler.java
+++ src/main/java/com/deliveryfood/core/web/ApiRetirementHandler.java
@@ -1,5 +1,6 @@
 package com.deliveryfood.core.web;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -7,15 +8,14 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class ApiDeprecationHandler implements HandlerInterceptor {
+public class ApiRetirementHandler implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
         if (request.getRequestURI().startsWith("/v1/")) {
-            response.addHeader("X-Deliveryfood-Deprecated",
-                    "Essa versão da API está depreciada e deixará de existir a partir de 01/01/2026."
-                            + " Use a versão mais atual da API.");
+            response.setStatus(HttpStatus.GONE.value()); // Erro (GONE): 410
+            return false;
         }
 
         return true;
