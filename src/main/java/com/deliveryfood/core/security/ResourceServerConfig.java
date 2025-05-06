fbb37480a6051d977b6b8d0c55f6695e742c2340
petterson <petterson@email.com>
2025-05-06 16:01:38 -0300

    Configurando o Resource Server com token JWT

--- src/main/java/com/deliveryfood/core/security/ResourceServerConfig.java
+++ src/main/java/com/deliveryfood/core/security/ResourceServerConfig.java
@@ -13,14 +13,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfig {
 
-    @Value("${spring.security.oauth2.resourceserver.opaque-token.introspection-uri}")
-    private String introspectionUri;
-
-    @Value("${spring.security.oauth2.resourceserver.opaque-token.client-id}")
-    private String clientId;
-
-    @Value("${spring.security.oauth2.resourceserver.opaque-token.client-secret}")
-    private String clientSecret;
+    // @Value("${spring.security.oauth2.resourceserver.opaque-token.introspection-uri}")
+    // private String introspectionUri;
+    
+    
+    // @Value("${spring.security.oauth2.resourceserver.opaque-token.client-id}")
+    // private String clientId;
+    
+    // @Value("${spring.security.oauth2.resourceserver.opaque-token.client-secret}")
+    // private String clientSecret;
+    
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
 
     @Bean
     public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception{
@@ -32,10 +36,9 @@ public class ResourceServerConfig {
             .csrf(csrf -> csrf.disable())
             .cors(cors -> {}) // configuração padrão
             .oauth2ResourceServer(oauth2 -> oauth2
-                .opaqueToken(token -> {
-                    token.introspectionUri(introspectionUri);
-                    token.introspectionClientCredentials(clientId, clientSecret);
-                })
+                    .jwt(jwt -> jwt
+                    .jwkSetUri(jwkSetUri) // substitua pelo URI do seu JWK Set
+                )
             );
 
         return http.build();
