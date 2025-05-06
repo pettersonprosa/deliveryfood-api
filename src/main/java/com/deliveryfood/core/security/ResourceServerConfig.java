e44a06210d15b41b2ab83629ef263525637ea30c
petterson <petterson@email.com>
2025-05-06 11:33:48 -0300

    Configurando o Resource Server com token opaco

---
+++ src/main/java/com/deliveryfood/core/security/ResourceServerConfig.java
@@ -0,0 +1,44 @@
+package com.deliveryfood.core.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class ResourceServerConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.opaque-token.introspection-uri}")
+    private String introspectionUri;
+
+    @Value("${spring.security.oauth2.resourceserver.opaque-token.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.resourceserver.opaque-token.client-secret}")
+    private String clientSecret;
+
+    @Bean
+    public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception{
+        http
+            .authorizeHttpRequests(authz -> authz
+                .requestMatchers("/oauth2/**").authenticated()
+                .anyRequest().permitAll()
+            )
+            .csrf(csrf -> csrf.disable())
+            .cors(cors -> {}) // configuração padrão
+            .oauth2ResourceServer(oauth2 -> oauth2
+                .opaqueToken(token -> {
+                    token.introspectionUri(introspectionUri);
+                    token.introspectionClientCredentials(clientId, clientSecret);
+                })
+            );
+
+        return http.build();
+    }
+
+}
