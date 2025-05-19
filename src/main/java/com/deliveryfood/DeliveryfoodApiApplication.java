480b5c1fb8a63c62c51051196a21715c01608cf1
petterson <petterson@email.com>
2025-05-19 15:27:04 -0300

    Fix: Corrige redirecionamento após login (Closes #2)

--- src/main/java/com/deliveryfood/DeliveryfoodApiApplication.java
+++ src/main/java/com/deliveryfood/DeliveryfoodApiApplication.java
@@ -5,11 +5,13 @@ import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 import com.deliveryfood.core.io.Base64ProtocolResolver;
 import com.deliveryfood.infrastructure.repository.CustomJpaRepositoryImpl;
 
 @SpringBootApplication
+@EnableRedisHttpSession
 @EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class DeliveryfoodApiApplication {
 
